075b8f39fbf369296e994d4b2dc57168c3f2a033
angelegito <angelegito@hotmail.com>
2022-10-06 22:46:17 -0300

    modificação do nome na mensagem

--- aula08/index.js
+++ aula08/index.js
@@ -2,7 +2,7 @@ function bemVinda () {
     //! diz que se não existir algo faça isso. A ! serve como negação.
     if(!document.querySelector('#bemVinda')){
         const mensagem = document.createElement('h1'); //=cria uma tag <h1>
-        mensagem.innerHTML = 'Bem Vinda Angel Egito'; //<h1> nova mensagem </h1>
+        mensagem.innerHTML = 'Bem Vinda Angélica Egito'; //<h1> nova mensagem </h1>
         mensagem.id = 'bemVinda';
         document.querySelector('#mensagem').appendChild(mensagem); //E adiciona na interaçãoa noma mensagem criada. Para chamar um id colocasse o # antes e para chamar uma class colocasse um . antesde chama-la. 
     }
