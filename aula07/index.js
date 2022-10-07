08962e2e515cb60c9aeeddc6c097397c8165ec31
angelegito <angelegito@hotmail.com>
2022-10-06 22:02:09 -0300

    Subindo aula07 e aula08

---
+++ aula07/index.js
@@ -0,0 +1,43 @@
+
+/*function qualquerCoisa (param1, param2)
+
+//escopo da funcao
+
+const outraCoisa = (param1, param2, param3) => {
+    //escopo da funcao
+}
+
+qualquerCoisa(1, 2)
+outraCoisa(1, 2, 3)*/
+
+//let nome = 'Nath'
+// nome = 'Nathally Souza'
+
+ //fetch('https://pokeapi.co/api/v2/pokemon/ditto').then(resposta => console.log(resposta));
+ 
+//console.log(nome)
+
+fetch('https://cep.awesomeapi.com.br/53150400')
+.then(resposta => resposta.json())
+.then(dados => console.log(dados.address_name))
+
+//console.log(nome) ESTA É A LINHA QUE ESTAVA CAUSANDO O ERRO!
+
+
+function qualquer() {
+    return 2
+}
+
+console.log(qualquer())
+
+const curso = 'PretaLab'
+
+document.getElementById('aula').innerHTML = 'Tralhando com o DOM no JS'
+
+const novaDiv = document.createElement('div')
+
+novaDiv.innerHTML = 'Div criada com DOM'
+
+document.getElementById('aula').appendChild(novaDiv)
+
+
---
+++ aula08/index.js
@@ -0,0 +1,23 @@
+function bemVinda () {
+    //! diz que se não existir algo faça isso. A ! serve como negação.
+    if(!document.querySelector('#bemVinda')){
+        const mensagem = document.createElement('h1'); //=cria uma tag <h1>
+        mensagem.innerHTML = 'Bem Vinda Angel'; //<h1> nova mensagem </h1>
+        mensagem.id = 'bemVinda';
+        document.querySelector('#mensagem').appendChild(mensagem); //E adiciona na interaçãoa noma mensagem criada. Para chamar um id colocasse o # antes e para chamar uma class colocasse um . antesde chama-la. 
+    }
+}
+
+
+function mudaCor () {
+    let corFonte = document.querySelector('#bemVinda').style.color
+    console.log(corFonte)
+    if (corFonte == '' || corFonte == 'rgb(0, 0, 0)' ) {
+        document.querySelector('#bemVinda').style.color = '#A020F0'
+    } else {
+        document.querySelector('#bemVinda').style.color = '#000000'
+    }
+    
+}
+//Manipulando o CSS no Js.
+
